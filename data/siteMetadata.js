3e3847b20608db7002dfd7067980fa2886d2b822
signalmindlab <k12j35c17@gmail.com>
2025-12-11 18:37:16 +0900

    remove load comments

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -70,7 +70,8 @@ const siteMetadata = {
     // content security policy in the `next.config.js` file.
     // Select a provider and use the environment variables associated to it
     // https://vercel.com/docs/environment-variables
-    provider: 'giscus', // supported providers: giscus, utterances, disqus
+    // provider: 'giscus', // supported providers: giscus, utterances, disqus
+    provider: '',
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
